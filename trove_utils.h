0b0cebf913a0f7f04afda605cc3b5b09297ede68
KyleCapelli <66346910+KyleCapelli@users.noreply.github.com>
2022-10-12 13:53:25 +0800

    Delete trove_utils.h

--- trove_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef trove_header
-#define trove_header
-
-char** find_files(char* directory_name);
-bool index_file(char* file_name, char* trove_file, int min_length);
-void remove_index(char* file_name, char* trove_file);
-
-#endif
